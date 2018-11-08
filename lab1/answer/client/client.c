de25eb22a7b8d6fbc4cf6a70463efbcded0d50ab
lionem2018 <lionem2018@gmail.com>
2018-11-08 08:54:03 -0800

    add comments

--- lab1/answer/client/client.c
+++ lab1/answer/client/client.c
@@ -16,68 +16,68 @@
 
 int read_msg(SSL *ssl, unsigned char **msgBuf, PACKET_HEADER **header);
 ssize_t readn(SSL *ssl, unsigned char *buf, size_t nbytes);
-ssize_t writen(SSL *ssl, unsigned char *buf, size_t nbytes);
+ssize_t writen(SSL *ssl, unsigned char *buf, size_t nbytes);  // print error message and exit
 void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-	int sock;
-	struct sockaddr_in serv_adr;
-	int length, recv_len = 0;
-	unsigned char *message, *sendBuf = NULL;
-	char id[MAX_ID_LEN] = {0}, password[MAX_PWD_LEN] = {0};
+	int sock;                                                   // socket identifier ( return value of socket() function )
+	struct sockaddr_in serv_adr;                                // create address structure for server
+	int length, recv_len = 0;                                   // length: send data length, recv_len: receive data length
+	unsigned char *message, *sendBuf = NULL;                    // message: receive message, sendBuf: send message to server
+	char id[MAX_ID_LEN] = {0}, password[MAX_PWD_LEN] = {0};     // id, password: input id, password from user
 
 	///////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////
-	SSL_CTX *ctx;
+	SSL_CTX *ctx;       // framework for TLS/SSL connection
 	SSL *ssl;
-	X509 *server_cert;
+	X509 *server_cert;  // standard defining the format of the public key certificates
 	///////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////
 
-	PACKET_HEADER *header;
-	LOGIN_REQ *loginReq;
+	PACKET_HEADER *header;   // header pointer for packet
+	LOGIN_REQ *loginReq;     // login request packet
 
-	if(argc!=3) {
+	if(argc!=3) {            // check argumets (IP, PORT)
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
 
-	sock=socket(PF_INET, SOCK_STREAM, 0);
-	if(sock==-1)
+	sock=socket(PF_INET, SOCK_STREAM, 0);    // create socket (PF_INET: IPv4 Protocol(usually use for socket), SOCK_STREAM:use STREAM for TCP, 0: usually)
+	if(sock==-1)                             // fail of creating socket
 		error_handling("socket() error");
 
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET;
-	serv_adr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_adr.sin_port=htons(atoi(argv[2]));
+	memset(&serv_adr, 0, sizeof(serv_adr));        // initialize server address structure (all value is 0)
+	serv_adr.sin_family=AF_INET;                   // set sin_family value (AF_INET: IPv4 Protocol(usually use for address))
+	serv_adr.sin_addr.s_addr=inet_addr(argv[1]);   // set server IP address (inet_addr(char * <String IP address>): 'char *' type -> 'long' type )
+	serv_adr.sin_port=htons(atoi(argv[2]));        // set server port ( htons(int <port number>): change byte order 0x123456 -> 0x563412 => for Little-Endian host)
 
-	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)  // try to connect (return value -1 is fail)
 		error_handling("connect() error!");
 	else
-		puts("Connected...........");
+		puts("Connected..........."); 
 
 	///////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////   
 	SSL_load_error_strings();
 	SSLeay_add_ssl_algorithms();
 
-	ctx = SSL_CTX_new(TLSv1_2_client_method());
+	ctx = SSL_CTX_new(TLSv1_2_client_method());   // create SSL_CTX(only use TLSv1.2)
 
-	ssl = SSL_new(ctx);
+	ssl = SSL_new(ctx);                           // create SSL(with setting ctx)
 
-	SSL_set_fd(ssl, sock);
-	if(SSL_connect(ssl) == -1){
+	SSL_set_fd(ssl, sock);                        // link SSL and socket
+	if(SSL_connect(ssl) == -1){                   // try to connect SSL
 		return -1;
 	}
 
-	server_cert = SSL_get_peer_certificate(ssl);
+	server_cert = SSL_get_peer_certificate(ssl);  // get peer(server)'s x509 certificate for ssl
 
 	printf("\nServer certificate:\n");
 	printf("subject: %s\n", X509_NAME_oneline(X509_get_subject_name(server_cert), 0, 0));
 	printf("issuer: %s\n", X509_NAME_oneline(X509_get_issuer_name(server_cert), 0, 0));
 
-	X509_free(server_cert);
+	X509_free(server_cert);  //free x509 certificate
 	///////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////
 
